01c09da7d01ac35cb23cb7bf5bf41c5d4da10d6c
Favour okerri <favourokerri767@gmail.com>
2023-05-24 02:36:06 -0700

    sub.c

--- monty.h
+++ monty.h
@@ -78,6 +78,7 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 /* stream */
 void fs_close(int status, void *arg);
 #endif
