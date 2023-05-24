792ad97c09417c6670fcce18b7e2d78130c1ed3b
Favour okerri <favourokerri767@gmail.com>
2023-05-24 03:02:14 -0700

    sub.c

--- sub.c
+++ sub.c
@@ -1,26 +1,26 @@
 #include "monty.h"
-
 /**
- * sub - sub the top two elements of the stack
- * @stack: double pointer to the top of the stack
- * @line_number: script line number
+ * sub - Subtract the top two elements of the stack
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Script line number
  *
  * Return: void
  */
-
 void sub(stack_t **stack, unsigned int line_number)
 {
-	int n = 0;
+	stack_t *top, *second_top;
 
 	if (var.qs_len < 2)
 	{
 		fprintf(stderr,
-		"L%u: can't sub, stack too short\n",
-		line_number);
+			"L%u: can't sub, stack too short\n",
+			line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	n -= (*stack)->n;  /* Subtract top element from the second top element */
+	top = *stack;
+	second_top = top->next;
+
+	top->n -= second_top->n;
 	pop(stack, line_number);
-	(*stack)->n -= n;  /* Store the result in the second top element */
 }
