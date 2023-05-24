0b0417eb7dc7b789c6389ed99dbf490a6885fb68
Favour okerri <favourokerri767@gmail.com>
2023-05-24 03:53:09 -0700

    sub.c

--- sub.c
+++ sub.c
@@ -13,16 +13,14 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr,
-			"L%u: can't sub, stack too short\n",
-			line_number);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	top = *stack;
 	second_top = top->next;
-
 	second_top->n -= top->n;
 
 	pop(stack, line_number);
 }
+
