1ec1bdfce9885d6cd1ef573c141add5502c4aaad
Favour okerri <favourokerri767@gmail.com>
2023-05-24 00:15:12 -0700

    nop.c

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - no operation performed
+ * @stack: double pointer to the top of stack
+ * @line_number: line number of current instruction
+ *
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
